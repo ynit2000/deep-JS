34a05946ec893ecfcc94d69d9a18b0c6b053514f
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-02-05 15:12:16 +0530

    main

---
+++ async&EventLoop.js
@@ -0,0 +1,26 @@
+// flow of work : gec > call stack > JS Engine > Browser 
+
+// Behind the scense in Browser : it's have "Web APLs": acces thr'o "window"
+
+// setTimeout()
+// Dom APIs
+// fetch()
+// localStorage
+// console
+// Location
+
+
+
+
+// Event loop is always clecked  continuse call stack is empty or not if yes then 
+
+// checked task as print in MIcrotask Quene is 1st rank promterty  (fetch, promise, mumatationObervers)
+// checked task as print in callback Quene is 2st rank promterty (other everytask here)
+
+
+// Problem in STarvation is case when :
+
+// 1. MIcrotask Quene is contineue in loop for 
+// 2. callback never get chanse to exteced 
+
+
