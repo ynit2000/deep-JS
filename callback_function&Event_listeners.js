3d020960987ef0f58e1a8b86cffee17043c5964d
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-02-05 10:19:53 +0530

    Callback&EventListeners

---
+++ callback_function&Event_listeners.js
@@ -0,0 +1,15 @@
+// What is a callback Function in JS 
+
+// JavaScript is a synchronous and singel-threded language
+
+// Blocking the main thread 
+
+// Power of Callbacks?
+
+// Deep about Event Listeners
+
+// Closures Demo with Event Listeners
+
+// Scope Demo with Event Listeners
+
+// Garbage Collection & removeEventListeners 
