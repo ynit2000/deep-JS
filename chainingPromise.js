45093d6abc4165548a8b03fdcf7be3bcacf5f742
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-02-13 12:38:44 +0530

    Creating-Chaining-ErrorHandingInPromise

--- chainingPromise.js
+++ chainingPromise.js
@@ -1,11 +1,18 @@
 const cart = ["shoes", "pants", "kurta"];
 
-
 const promise = createOrder(cart);  // orderId
+console.log(promise);
 
 promise.then(function () {
     proceedToPayment(orderId);
+    // proceedToPayment(orderId)
+})
+
+// error handing
+.catch(function (err){
+    console.log(err.message);
 });
+
 
 // imp part this "Creating a Promise" we can resolve, reject a promise
 
@@ -18,17 +25,20 @@ function createOrder(cart) {
         if (!validateCart(cart)) {
             const err = new Error("Cart is not valid");
             reject(err);
-
-            // logic for createOrder
-            const orderId = "12345";
-            if (orderId) {
-                resolve(orderId);
-            }
         }
+        // logic for createOrder
+        const orderId = "12345";
+        if (orderId) {
+            setTimeout(function () {
+                resolve(orderId);
+            }, 5000);
+
+        }
+
     });
     return pr;
 }
 
 function validateCart(cart) {
-    return true;
+    return false;
 }
