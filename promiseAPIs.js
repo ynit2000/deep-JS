e8d80b10fc3527cf40a86376135764b5d6390486
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-02-18 11:04:37 +0530

    PromiseAPIs

--- promiseAPIs.js
+++ promiseAPIs.js
@@ -8,13 +8,21 @@
                 //   after 3s it's give [val1, val2, val3]
                 //   wait for all of their to finish 
 
-                
+
                  //   take ex. if promise2 is fail after 1s 
-                //   immediately this going to show error 
+                //   immediately when fail this going to show error 
 
 
 
 //                 2. Promise.allSettled() 
+                    //   3 user making api call parallel call at same time
+                  //   const promises = [promise1, promise2, promise3];
+                                     //   3s Y        1s  N       2s Y
+                 // Promise.allSettled(promises)
+                //  wait for all Promise to settled after 3sec 
+                //   it give  array [val1,error,val3]
+
 //                 3. Promise.race() 
+
 //                 4. Promise.any()
 
