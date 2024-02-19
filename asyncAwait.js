2d60406ddefc31893dfb25c52daf8039d1a1c644
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-02-19 10:34:32 +0530

    asyncAwait

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,29 +1,34 @@
 /** 
 *what is async ?  --> is keyword to create async function {what is async func. -->which are " always return a Promise"}
+
+              const p = new Promise((resolve, reject) => {
+                  resolve("Promise Resolved Value!!");
+              });
+              
+              // always return a Promise 
+              async function getMe(){
+                  // return "nix";
+                  
+                  return p;
+              }
+              
+              const dataPromise = getMe();
+              // console.log(data);
+              
+              dataPromise.then((res) => console.log(res));
+
 *what is await ?
+
 *How async await works behind the scenes ?
 *Examples of using async/ await 
 *Erorr Handling
 *Interviews
 *Async await vs Promise.then/.catch
 *
+* Async and await used for handing Proimse *
 */
 
 const p = new Promise((resolve, reject) => {
     resolve("Promise Resolved Value!!");
 });
 
-// always return a Promise 
-async function getMe(){
-    // return "nix";
-    
-    return p;
-
-
-}
-
-const dataPromise = getMe();
-
-// console.log(data);
-
-dataPromise.then((res) => console.log(res));
